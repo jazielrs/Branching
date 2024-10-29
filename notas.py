a340094a37b7bffa16ed92e7a4030b46f021c622
Kendall Sánchez <117062888+Kensc19@users.noreply.github.com>
2024-10-28 21:58:54 -0600

    new release

--- notas.py
+++ notas.py
@@ -1,13 +1,27 @@
 import tkinter as tk
 from tkinter import messagebox
 
-ventana = tk.Tk()
-ventana.title("Notas Rápidas")
-text_area = tk.Text(ventana, width=40, height=10)
-text_area.pack()
-
 def guardar_nota():
     nota = text_area.get("1.0", tk.END)
     with open("nota.txt", "w") as archivo:
         archivo.write(nota)
     messagebox.showinfo("Guardar Nota", "Nota guardada con éxito.")
+def cargar_nota():
+    try:
+        with open("nota.txt", "r") as archivo:
+            nota = archivo.read()
+            text_area.delete("1.0", tk.END)  # Limpiar el área de texto
+            text_area.insert(tk.END, nota)  # Cargar la nota
+    except FileNotFoundError:
+        messagebox.showwarning("Cargar Nota", "No se encontró el archivo de nota.")
+        
+ventana = tk.Tk()
+ventana.title("Notas Rápidas")
+text_area = tk.Text(ventana, width=40, height=10)
+text_area.pack()
+# Botones
+tk.Button(ventana, text="Guardar Nota", command=guardar_nota).pack()
+tk.Button(ventana, text="Cargar Nota", command=cargar_nota).pack()
+
+ventana.mainloop()
+
