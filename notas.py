3291bb8c8a39d38481434916d64cbb11391e6f0f
Aarón Chacón <aaronchacon2601@gmail.com>
2024-10-28 21:43:43 -0600

    guardar

--- notas.py
+++ notas.py
@@ -5,3 +5,9 @@ ventana = tk.Tk()
 ventana.title("Notas Rápidas")
 text_area = tk.Text(ventana, width=40, height=10)
 text_area.pack()
+
+def guardar_nota():
+    nota = text_area.get("1.0", tk.END)
+    with open("nota.txt", "w") as archivo:
+        archivo.write(nota)
+    messagebox.showinfo("Guardar Nota", "Nota guardada con éxito.")
